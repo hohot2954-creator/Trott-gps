0e2933f89b354e952ea7346dcb0c7b677c3dd50d
hohot2954-creator <hohot2954@gmail.com>
2026-09-09 07:27:16 +0200

    Refactor main function to use TrottGpsApp
    Refactored main function to use TrottGpsApp widget for better structure and maintainability.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(child: Text('Appli Trott GPS prête !')),
-    ),
-  ));
+  runApp(const TrottGpsApp());
+}
+
+class TrottGpsApp extends StatelessWidget {
+  const TrottGpsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trott GPS',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Trott GPS - Fonctionnel !', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
+  }
 }
